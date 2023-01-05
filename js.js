1e4fd1dbe658f744fb809d710462bb6a092d5bf3
Nikita Gurenko <gurenkon94@icloud.com>
2023-01-05 18:05:54 +0200

    terms

--- js.js
+++ js.js
@@ -1,24 +1,29 @@
 'strict';
-const numberOfFilms = +prompt('Сколько фильмов вы псомтрели?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+/* if (4==4) {
+    console.log('Ok!');
+} else {
+    console.log('Error!');
+}
 
-const a = prompt('Один из просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+const num = 50;
 
+(num === 50) ? console.log('Ok!') : console.log('Error'); */
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
-
-      
-
+const num = 50;
+// switch конструкци котора поддерживает проверку нескольких  условий
+switch (num) {
+    case 49:
+        console.log('неверно');
+        break;
+    case 100:
+        console.log('неверно');
+        break;
+    case 50:
+        console.log('В точку!');
+        break;
+    default: 
+        console.log('Не в этот раз');
+        break;
+    
+}
