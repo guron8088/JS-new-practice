9d94bf8e66a61cfb98d2dafcfea8a1fdb3a06303
Nikita Gurenko <gurenkon94@icloud.com>
2023-01-15 18:22:32 +0200

    second task

--- js.js
+++ js.js
@@ -1,34 +1,57 @@
-/* 'use strict';
-const hamburger = true;
-const fries = true;
-
-if (hamburger && fries) {
-    console.log('Сыт!');
+'use strict';
+//1.При помощи цикла выведите числа от 5 до 10 в консоль. 5 и 10 включительно. Цикл можно использовать любой
+function firstTask() {
+    for (let i = 5; i <= 10; i++) {
+        console.log(i);
+    }
 }
 
-console.log((hamburger && fries)); */
-//результат логических операций это какоето логическое булевое значение.
+//2.При помощи цикла for вывести числа от 20 до 10 в консоль. В обратном порядке (20, 19, 18...). Когда цикл дойдет до числа 13 - остановить весь цикл
+function secondTask() {
+    for (let i = 20; i >= 10; i--) {
+        if (i === 13) break;
+        console.log(i);
+    
+    }
+}
+secondTask();
 
-//если одно из блюд будет отсутствовать получим фолс
-/* const hamburger = true;
-const fries = false;
 
-if (hamburger && fries) {
-    console.log('Сыт!');
+//3. При помощи цикла for выведите чётные числа от 2 до 10 включительнo
+function thirdTask() {
+    for (let i = 2; i <= 10; i++) {
+        if (i % 2 === 0) {
+            console.log(i);
+        }
+    }
 }
 
-console.log((hamburger && fries)); */
-//получим false
+thirdTask();
 
-const hamburger = 2;
-const fries = 1;
+function fourthTask() {
+    let i = 2;
 
-if (hamburger === 3 && fries) {
-    console.log('Все сыты!');
-} else {
-    console.log('Мы уходим!');
+    while (i <= 16) {
+        if (i % 2 === 0) {
+            i++;
+            continue;
+        } else {
+            console.log(i);
+        }
+        i++;
+    }
+
 }
 
+fourthTask();
 
-//hamburger === 3 эта часть выполнетс первой, далее выполнеетс сравнение && 
-//лева часть правда, права часть неправда
+//5.Заполните массив цифрами от 5 до 10 включительно. Помните, что элементы массива можно сформировать так же, как и обращаться к ним: arr[0]
+function fifthTask() {
+    const arrayOfNumbers = [];
+    for (let i = 5; i < 11; i++) {
+        arrayOfNumbers[i-5] = i;
+    }  
+    console.log(arrayOfNumbers);
+    return arrayOfNumbers;
+}
+fifthTask();
